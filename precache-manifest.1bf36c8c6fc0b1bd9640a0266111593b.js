b31aa80c261696b4b9c60a33107d2ed08e70d909
Xavi Torné <xavitorne@gmail.com>
2020-06-08 11:42:32 +0200

    first test murialdo

---
+++ precache-manifest.1bf36c8c6fc0b1bd9640a0266111593b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a68ce22a4baae25838085e49d2e5a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eab58349cad2d33ce4af",
+    "url": "./static/css/2.295226a9.chunk.css"
+  },
+  {
+    "revision": "6f780d704cd305126d2c",
+    "url": "./static/css/main.346e6c24.chunk.css"
+  },
+  {
+    "revision": "eab58349cad2d33ce4af",
+    "url": "./static/js/2.46197053.chunk.js"
+  },
+  {
+    "revision": "ad5ddeabaa0140119c6a586dbf92b967",
+    "url": "./static/js/2.46197053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f780d704cd305126d2c",
+    "url": "./static/js/main.f87e91db.chunk.js"
+  },
+  {
+    "revision": "db410e9c4bd8bf868190",
+    "url": "./static/js/runtime-main.b890c8c5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
